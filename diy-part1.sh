c21571917feb01720c092a87bb87260a923af66e
mylucien <53183558+mylucien@users.noreply.github.com>
2021-01-07 01:01:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,9 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#加入helloworld
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 #other-plugins
 		if [[ -e package/other-plugins ]]; then
 			echo ""
