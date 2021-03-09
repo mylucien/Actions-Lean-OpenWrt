997a62f2d2553c93981317dcf8dfa55ec79dd38c
mylucien <53183558+mylucien@users.noreply.github.com>
2021-03-10 00:36:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/tty228/luci-app-serverchan.git package/other-plugins/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/other-plugins/luci-app-adguardhome
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
