a74abd1625b22f0b8a59f32b81e184e2e3fdb6e4
mylucien <53183558+mylucien@users.noreply.github.com>
2021-07-19 12:37:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/tty228/luci-app-serverchan.git package/other-plugins/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/other-plugins/luci-app-adguardhome
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/other-plugins/chinadns-ng
-git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/other-plugins/luci-app-chinadns-ng
+git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/other-plugins/luci-app-chinadns-ng
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
