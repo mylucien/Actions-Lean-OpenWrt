85fe988b965ec07b4484e08779c4fd56f030ea0c
mylucien <53183558+mylucien@users.noreply.github.com>
2021-10-20 11:25:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,3 +3,8 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/othe
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/other-plugins/chinadns-ng
 git clone https://github.com/izilzty/luci-app-chinadns-ng.git package/other-plugins/luci-app-chinadns-ng
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+wget https://raw.githubusercontent.com/mylucien/Actions-Lean-OpenWrt/main/luci-app-bridge.tar.gz
+tar -zxvf luci-app-bridge.tar.gz -C ./feeds/luci/applications
+pushd ./package/feeds/luci
+ln -s ../../../feeds/luci/applications/luci-app-bridge luci-app-bridge
+popd
