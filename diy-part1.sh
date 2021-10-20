e75c6e200631eeadbfd836ccbeaa02f59670d63e
mylucien <53183558+mylucien@users.noreply.github.com>
2021-10-20 15:48:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/Lancenas/Actions-Lean-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+# fw876/helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# other-plugins
 git clone https://github.com/tty228/luci-app-serverchan.git package/other-plugins/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/other-plugins/luci-app-adguardhome
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/other-plugins/chinadns-ng
 git clone https://github.com/izilzty/luci-app-chinadns-ng.git package/other-plugins/luci-app-chinadns-ng
-echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
