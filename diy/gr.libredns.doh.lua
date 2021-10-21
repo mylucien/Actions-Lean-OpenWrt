e6fee1c69ad87a67375bf794ba6599db1e93b0ee
mylucien <53183558+mylucien@users.noreply.github.com>
2021-10-21 16:04:53 +0800

    Add files via upload

---
+++ diy/gr.libredns.doh.lua
@@ -0,0 +1,8 @@
+return {
+	name = "myDNS",
+	label = _("myDNS"),
+	resolver_url = "https://www.ccxx.live:8443/dns-query",
+	bootstrap_dns = "119.29.29.29",
+	help_link = "https://www.ccxx.live/",
+	help_link_text = "www.ccxx.live"
+}
