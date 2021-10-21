b341694d71173dd1a220d87378ec894cd801e299
mylucien <53183558+mylucien@users.noreply.github.com>
2021-10-21 17:27:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,3 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/other-plugin
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/other-plugins/luci-app-adguardhome
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/other-plugins/chinadns-ng
 git clone https://github.com/izilzty/luci-app-chinadns-ng.git package/other-plugins/luci-app-chinadns-ng
-git clone https://github.com/mylucien/luci-app-usbnet.git package/other-plugins/luci-app-usbnet
