82b915237c49934232919dc190b648dadc1a0d46
mylucien <53183558+mylucien@users.noreply.github.com>
2021-10-30 23:12:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,6 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.defa
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 
 # other-plugins
-git clone https://github.com/tty228/luci-app-serverchan.git package/other-plugins/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/other-plugins/luci-app-adguardhome
 git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/other-plugins/chinadns-ng
 git clone https://github.com/izilzty/luci-app-chinadns-ng.git package/other-plugins/luci-app-chinadns-ng
